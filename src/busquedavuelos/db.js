43e9e289e5126d5a3538dbbef0fcce3528412233
Diego Quinteros <diegoquinteros02.04.97@gmail.com>
2018-10-20 20:33:46 -0600

    db.js

---
+++ src/busquedavuelos/db.js
@@ -0,0 +1,64 @@
+{
+ ticket_vuelos: [
+      {
+        id: '',
+        origen: '',
+        destino: '',
+        fecha_salida: '',
+        fecha_llegada: '',
+        clase: '',
+        tipo: '',
+        tipo_pasajero: '',
+        aerolinea: '',
+        escalas: [],
+      }
+    ],
+  paises: [
+      {
+        id: '',
+        nombre: '',
+        ciudades: [
+          {
+          nombre: '',
+          aeropuertos: [
+              nombre: ''
+            ]
+          }
+        ]
+      }
+    ],
+    //Clases: economica etc
+    clases: [
+      {
+        id: '',
+        nombre: ''
+      }
+    ],
+    //Tipo: solo ida etc
+    tipo: [
+      {
+        id: '',
+        nombre: ''
+      }
+    ],
+    //adulto, joven etc
+    tipo_pasajero: [
+      {
+        id: '',
+        nombre: ''
+      }
+    ],
+    aerolineas: [
+      {
+        id: '',
+        nombre: '',
+        telefono: ''
+      }
+    ]
+}
+
+//Responsabilidades
+//Diego: frontend
+//Gustavo: backend
+//Lizama: doc
+//Gabo: doc
